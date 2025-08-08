00f0b2643d0a5778a9881b14231793dae6d3f3c9
sansvirus <129865458+sansvirus@users.noreply.github.com>
2025-08-08 20:18:18 +0200

    Update board_configuration.cpp

--- board_configuration.cpp
+++ board_configuration.cpp
@@ -10,4 +10,11 @@ void setBoardDefaultConfiguration() {
     setDefaultHellenAtPullUps();
     setHellenMMbaro();
     setHellenMegaEnPin();
+	// SPI2 for on-board EGT
+	engineConfiguration->is_enabled_spi_2 = true;
+	// MOSI not needed, we have one-way communication here
+	engineConfiguration->spi2misoPin = Gpio::B14;
+	engineConfiguration->spi2sckPin = Gpio::B13;
+	engineConfiguration->max31855_cs[0] = Gpio::B12;
+	engineConfiguration->max31855spiDevice = SPI_DEVICE_2;
 }
