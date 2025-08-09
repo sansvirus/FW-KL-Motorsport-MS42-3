3df2478fa3a808c9d0e4353ac772a71cf07b0ee9
sansvirus <129865458+sansvirus@users.noreply.github.com>
2025-08-09 08:11:23 +0200

    Update board_configuration.cpp

--- board_configuration.cpp
+++ board_configuration.cpp
@@ -15,15 +15,11 @@ void setBoardConfigOverrides() {
 // board-specific configuration setup
 void setBoardDefaultConfiguration() {
     setHellenMMbaro();
-	setupEGT();
 	
-	//EGT overwrites
-
 	engineConfiguration->spi2sckPin = Gpio::B13;
 	engineConfiguration->spi2misoPin = Gpio::B14;
 	engineConfiguration->spi2mosiPin = Gpio::B15;
 	engineConfiguration->is_enabled_spi_2 = true;
-
 	engineConfiguration->max31855spiDevice = SPI_DEVICE_2;
 	engineConfiguration->max31855_cs[0] = Gpio::B12;
 }
