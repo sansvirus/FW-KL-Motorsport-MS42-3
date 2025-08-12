0f55f90b3da64c601d5cc0078e8a43c5d82e33e2
sansvirus <129865458+sansvirus@users.noreply.github.com>
2025-08-12 17:31:58 +0200

    Update board_configuration.cpp

--- board_configuration.cpp
+++ board_configuration.cpp
@@ -2,29 +2,13 @@
 #include "hellen_meta.h"
 #include "defaults.h"
 
-// board-specific configuration setup
-void setBoardDefaultConfiguration() {
-    setHellenVbatt();
-    setHellenCan();
-    setDefaultHellenAtPullUps();
-    // engineConfiguration->injectionPins[0] = Gpio::F13;
-    // engineConfiguration->ignitionPins[0] = Gpio::E15;
 
-//   engineConfiguration->triggerInputPins[0] = Gpio::B1;
-//	engineConfiguration->triggerInputPins[1] = Gpio::Unassigned;
-
-//	engineConfiguration->map.sensor.hwChannel = EFI_ADC_3;
-
-//	engineConfiguration->clt.adcChannel = EFI_ADC_1;
-
-//	engineConfiguration->iat.adcChannel = EFI_ADC_2;
-
-
-    	// 5.6k high side/10k low side = 1.56 ratio divider
-  //  	engineConfiguration->analogInputDividerCoefficient = 1.56f;
-
-    	// 6.34k high side/ 1k low side
-//    	engineConfiguration->vbattDividerCoeff = (6.34 + 1) / 1;
-
-//	engineConfiguration->adcVcc = 3.3f;
+void setBoardConfigOverrides() {
+	setHellenMegaEnPin();
+	hellenMegaSdWithAccelerometer();
+	setHellenCan();
+ 	setDefaultHellenAtPullUps();
+    setHellenMMbaro();
 }
+
+
