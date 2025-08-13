7c85fd5e4cbe815851c593df727f8488f4551d0c
sansvirus <129865458+sansvirus@users.noreply.github.com>
2025-08-13 10:20:01 +0200

    Update board_configuration.cpp

--- board_configuration.cpp
+++ board_configuration.cpp
@@ -3,14 +3,16 @@
 #include "defaults.h"
 
 
-static void customBoardDefaultConfiguration() {
-}
 
-void setup_KLM_MS43_board_overrides() {
-    KLM_MS43_DefaultConfiguration = customBoardDefaultConfiguration;
+static void KLM_MS43_boardoverrides() {
 	setHellenMegaEnPin();
 	hellenMegaSdWithAccelerometer();
 	setHellenCan();
  	setDefaultHellenAtPullUps();
     setHellenMMbaro();
+}
+
+void setup_custom_board_overrides() {
+	custom_board_DefaultConfiguration = KLM_MS43_boardDefaultConfiguration;
+	custom_board_ConfigOverrides =  KLM_MS43_boardConfigOverrides;
 }
