efbf44c2c1f1e5f46dec967d81c4d05ed6d7c812
GitHub git update Action <action@github.com>
2025-11-08 16:50:04 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_KL_Motorsport_MS42_MS43.h
+++ generated/controllers/generated/rusefi_generated_KL_Motorsport_MS42_MS43.h
@@ -2124,7 +2124,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.11.07.KL_Motorsport_MS42_MS43.3001969568"
+#define TS_SIGNATURE "rusEFI main.2025.11.08.KL_Motorsport_MS42_MS43.3001969568"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
