333019d013eff9f6f71efe4fcd3ef3d82a8f79d0
GitHub git update Action <action@github.com>
2025-12-05 16:57:22 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_KL_Motorsport_MS42_MS43.h
+++ generated/controllers/generated/rusefi_generated_KL_Motorsport_MS42_MS43.h
@@ -1546,7 +1546,7 @@
 #define SentInput_NONE 0
 #define show_tcu_gauges false
 #define show_vvt_output_pin true
-#define SIGNATURE_HASH 2410063653
+#define SIGNATURE_HASH 3866409250
 #define SIMULATOR_TUNE_BIN_FILE_NAME "generated/simulator_tune_image.bin"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_PREFIX "generated/simulator_tune_image"
 #define SIMULATOR_TUNE_BIN_FILE_NAME_SUFFIX ".bin"
@@ -2134,7 +2134,7 @@
 #define ts_show_wastegate_sensor true
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
-#define TS_SIGNATURE "rusEFI main.2025.12.04.KL_Motorsport_MS42_MS43.2410063653"
+#define TS_SIGNATURE "rusEFI main.2025.12.05.KL_Motorsport_MS42_MS43.3866409250"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- generated/controllers/generated/signature_KL_Motorsport_MS42_MS43.h
+++ generated/controllers/generated/signature_KL_Motorsport_MS42_MS43.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2410063653
-#define TS_SIGNATURE "rusEFI main.2025.12.04.KL_Motorsport_MS42_MS43.2410063653"
+#define SIGNATURE_HASH 3866409250
+#define TS_SIGNATURE "rusEFI main.2025.12.05.KL_Motorsport_MS42_MS43.3866409250"
