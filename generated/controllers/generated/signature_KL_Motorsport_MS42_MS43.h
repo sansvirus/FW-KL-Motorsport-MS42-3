a3b01680a8c4f0e3412789033367e1f09d091be7
GitHub git update Action <action@github.com>
2026-02-04 17:31:32 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_KL_Motorsport_MS42_MS43.h
+++ generated/controllers/generated/signature_KL_Motorsport_MS42_MS43.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 2279142733
-#define TS_SIGNATURE "rusEFI main.2026.02.03.KL_Motorsport_MS42_MS43.2279142733"
+#define SIGNATURE_HASH 1494306282
+#define TS_SIGNATURE "rusEFI main.2026.02.04.KL_Motorsport_MS42_MS43.1494306282"
